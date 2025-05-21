09908c99703be6d40eaff42c804c2e9f4e3fe4e5
gvladislav-52 <gvladislav_52@mail.ru>
2025-05-21 11:01:56 +0300

    start

---
+++ AnyApp3/AnyApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  AnyApp3App.swift
+//  AnyApp3
+//
+//  Created by gvladislav-52 on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnyApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
